341ef4d334a012e4471b4ad1fdedccc4bf72895f
CameronMackenzie99 <29843733+CameronMackenzie99@users.noreply.github.com>
2023-06-19 20:50:53 +0100

    Fix mobile overflow formatting

--- src/components/EditableCell.tsx
+++ src/components/EditableCell.tsx
@@ -23,16 +23,16 @@ export const EditableCell = ({
   };
 
   return (
-    <div className="relative rounded-md py-1 shadow-sm">
+    <div className="relative flex rounded-md py-2 shadow-sm">
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        <span className="text-gray-500 sm:text-sm">£</span>
+        <span className="invisible text-gray-500 sm:text-sm md:visible">£</span>
       </div>
       <input
         type="number"
         value={value.toFixed(0)}
         onChange={(e) => setValue(parseInt(e.target.value))}
         onBlur={onBlur}
-        className="block w-full rounded-md border-0 py-1.5 pl-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[hsl(216,92%,76%)] sm:text-sm sm:leading-6"
+        className="flex w-full rounded-md border-0 pl-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[hsl(216,92%,76%)] sm:py-1.5 sm:text-sm sm:leading-6 md:pl-7"
       />
     </div>
   );
